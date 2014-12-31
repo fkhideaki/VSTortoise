587e23fa75f83c725e087ec76afd61b1a6605e5d
qw_fuku <fkhideaki@gmail.com>
2015-01-01 00:47:20 +0900

    テストを追加

--- TGitAddin/TortoiseCore/TPTypeCehck.vb
+++ TGitAddin/TortoiseCore/TPTypeCehck.vb
@@ -7,17 +7,35 @@ Imports System.Diagnostics
 
 
 Public Class TPTypeCehck
-	Public Shared Function GetEngine(parent As DTE2) As TProcEngine
-		Dim dir As String = IDEPath.GetCurrentDocDir(parent)
-		If dir Is Nothing Then Return New TNull(parent)
+	Public Enum EngineType
+		None
+		Git
+		Svn
+	End Enum
+
+	Public Shared Function GetEngineType(dir As String) As EngineType
+		If dir Is Nothing Then Return EngineType.None
 
 		Do While (dir.Length >= 0)
-			If System.IO.Directory.Exists(dir + "\\" + ".git") Then Return New TGit(parent)
-			If System.IO.Directory.Exists(dir + "\\" + ".svn") Then Return New TSvn(parent)
+			If System.IO.Directory.Exists(dir + "\\" + ".git") Then Return EngineType.Git
+			If System.IO.Directory.Exists(dir + "\\" + ".svn") Then Return EngineType.Svn
 			Dim s As String = System.IO.Directory.GetParent(dir).ToString()
-			If s = dir Then Return New TNull(parent)
+			If s = dir Then Return EngineType.None
 		Loop
 
-		Return New TNull(parent)
+		Return EngineType.None
+	End Function
+
+	Public Shared Function GetEngine(parent As DTE2) As TProcEngine
+		Dim dir As String = IDEPath.GetCurrentDocDir(parent)
+		Dim t As EngineType = GetEngineType(dir)
+		Select Case t
+			Case EngineType.Git
+				Return New TGit(parent)
+			Case EngineType.Svn
+				Return New TSvn(parent)
+			Case Else
+				Return New TNull(parent)
+		End Select
 	End Function
 End Class
