2ded2e227dd733a0e4fe4ff130fc9239ed5d75af
qw_fuku <fkhideaki@gmail.com>
2014-12-31 23:45:26 +0900

    ソース整理

--- TGitAddin/LegacyVB/Tortoise.vb
+++ TGitAddin/LegacyVB/Tortoise.vb
@@ -8,35 +8,8 @@ Imports System.Diagnostics
 Public MustInherit Class Tortoise
 	Private DTE As DTE2 = Nothing
 
-	Protected MustOverride Function GetTPName() As String
-
 	Sub New(parent As DTE2)
 		DTE = parent
-	End Sub
-
-	Private Function CreateCmd(cmd As String, path As String) As String
-		Dim sc As String = " /command:" + cmd
-		Dim sp As String = " /path:" + """" + path + """"
-		Return GetTPName() + sc + sp + " /notempfile"
-	End Function
-
-	Private Function GetCurrentFilePath() As String
-		Dim filename As String = ""
-		If Not get_current_doc(DTE, filename) Then Return Nothing
-		Return filename
-	End Function
-
-	Private Function GetCurrentDirPath() As String
-		Dim dirname As String = ""
-		If Not get_current_doc_directory(DTE, dirname) Then Return Nothing
-		Return dirname
-	End Function
-
-	Private Sub ExecutePathCmd(cmd As String, path As String)
-		If path Is Nothing Then Return
-
-		Dim cmdline As String = CreateCmd(cmd, path)
-		DTE.ExecuteCommand("Tools.Shell", cmdline)
 	End Sub
 
 	' Diffウィンドウを表示
@@ -59,17 +32,43 @@ Public MustInherit Class Tortoise
 		ExecutePathCmd("log", GetCurrentDirPath())
 	End Sub
 
+	Protected MustOverride Function GetTPName() As String
+
+	Private Function CreateCmd(cmd As String, path As String) As String
+		Dim sc As String = " /command:" + cmd
+		Dim sp As String = " /path:" + """" + path + """"
+		Return GetTPName() + sc + sp + " /notempfile"
+	End Function
+
+	Private Function GetCurrentFilePath() As String
+		Dim filename As String = ""
+		If Not GetCurrentDoc(DTE, filename) Then Return Nothing
+		Return filename
+	End Function
+
+	Private Function GetCurrentDirPath() As String
+		Dim dirname As String = ""
+		If Not GetCurrentDocDir(DTE, dirname) Then Return Nothing
+		Return dirname
+	End Function
+
+	Private Sub ExecutePathCmd(cmd As String, path As String)
+		If path Is Nothing Then Return
+
+		Dim cmdline As String = CreateCmd(cmd, path)
+		DTE.ExecuteCommand("Tools.Shell", cmdline)
+	End Sub
+
 	'現在のドキュメントのフルパスを返す
-	Function get_current_doc(DTE As DTE2, ByRef filename As String) As Boolean
+	Private Function GetCurrentDoc(DTE As DTE2, ByRef filename As String) As Boolean
 		filename = DTE.ActiveDocument.FullName
-		If Not System.IO.File.Exists(filename) Then Return False
-		Return True
+		Return System.IO.File.Exists(filename)
 	End Function
 
 	'現在のドキュメントが保存されたディレクトリのフルパスを返す
-	Function get_current_doc_directory(DTE As DTE2, ByRef dirname As String) As Boolean
+	Private Function GetCurrentDocDir(DTE As DTE2, ByRef dirname As String) As Boolean
 		Dim filename As String = ""
-		If Not get_current_doc(DTE, filename) Then Return False
+		If Not GetCurrentDoc(DTE, filename) Then Return False
 		dirname = System.IO.Path.GetDirectoryName(filename)
 		Return True
 	End Function
